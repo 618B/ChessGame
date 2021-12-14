c945f96e2c2cbe51b2594f88f1e016763cc13026
ftTnemurtsni <ftTnemurtsni@gmail.com>
2021-12-14 16:44:07 +0300

    feat(ChessBorder): Add chessborder & chesspiece

---
+++ ChessBorder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame
+{
+    class ChessBorder
+    {
+        ChessPiece[,] _figures;
+        readonly int _size;
+
+        public ChessBorder(int size = 8)
+        {
+            _figures = new ChessPiece[size, size];
+            _size = size;
+        }
+
+        public bool IsFieldExists(int x, int y)
+        {
+            return (x > 0 && x < _size) && 
+                    (y > 0 && y < _size);
+        }
+
+        public ChessPiece this[int x, int y]
+        {
+            get
+            {
+                if (!IsFieldExists(x, y))
+                    throw new Exception("Out of border!");
+
+                return _figures[x, y];
+            }
+            set
+            {
+                if (!IsFieldExists(x, y))
+                    throw new Exception("Out of border!");
+
+                _figures[x, y] = value;
+            }
+        }
+        
+    }
+}
---
+++ ChessPiece.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame
+{
+    enum Side
+    {
+        White, Black
+    }
+
+    class ChessPiece
+    {
+        readonly Side _side;
+
+        public ChessPiece(Side side)
+        {
+            this._side = side;
+        }
+    }
+}
