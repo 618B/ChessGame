d43d04c5e7856054db070b9ab84285823b65154d
ftTnemurtsni <ftTnemurtsni@gmail.com>
2021-12-16 02:39:21 +0300

    feat(Moves): add move base class

---
+++ Move.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame
+{
+    abstract class Move
+    {
+        protected ChessBoard board;
+        protected ChessPiece piece;
+
+        public Move(ChessPiece piece,ChessBoard board)
+        {
+            this.board = board;
+            this.piece = piece;
+        }
+
+        public abstract void Execute();
+
+        public abstract void Undo();
+
+        public ChessPiece Piece => piece;
+    }
+}
