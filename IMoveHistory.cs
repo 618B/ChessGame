5a6c18e825c2efeb4f0391f4d32a865f86b6c350
ftTnemurtsni <ftTnemurtsni@gmail.com>
2021-12-16 01:47:27 +0300

    feat(ChessGame): add move history interface for move rules

---
+++ IMoveHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame
+{
+    interface IMoveHistory
+    {
+
+        bool LastMoveType<T>();
+
+        ChessPiece LastMovePiece { get; }
+
+        bool WasMoved(ChessPiece piece);
+    }
+}
