ad8a69862438842606386df19e98cdaf6772a928
ftTnemurtsni <ftTnemurtsni@gmail.com>
2021-12-15 16:20:55 +0300

    feat(MoveRules): Add attack and simple pawn moves

---
+++ MoveRules/PawnAttackMoveRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.MoveRules
+{
+    class PawnAttackMoveRule : StepMoveRule
+    {
+        public PawnAttackMoveRule(Point direction, ChessBoard board) : 
+            base(direction, board)
+        {
+        }
+
+        protected override bool IsMoveValid(Point startPosition, Point endPosition)
+        {
+            if (board[endPosition.X, endPosition.Y] == null)
+                return false;
+
+            return base.IsMoveValid(startPosition, endPosition);
+        }
+
+    }
+}
---
+++ MoveRules/PawnMoveRule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.MoveRules
+{
+    class PawnMoveRule : StepMoveRule
+    {
+
+        public PawnMoveRule(Point direction, ChessBoard board):
+            base(direction, board)
+        { }
+
+        protected override bool IsMoveValid(Point startPosition, Point endPosition)
+        {
+            if (board[endPosition.X, endPosition.Y] != null)
+                return false;
+
+            return base.IsMoveValid(startPosition, endPosition);
+        }
+    }
+}
