da7bc6819031ed3c341abea85a5a11ba555ae480
ftTnemurtsni <ftTnemurtsni@gmail.com>
2021-12-16 20:01:12 +0300

    fix(MoveRules): pawn start move rule work properly

--- MoveRules/PawnStartMoveRule.cs
+++ MoveRules/PawnStartMoveRule.cs
@@ -11,11 +11,13 @@ namespace ChessGame.MoveRules
     class PawnStartMoveRule : SlidingMoveRule
     {
         protected IMoveHistory history;
+        protected Point movement;
 
-        public PawnStartMoveRule(IMoveHistory history, Point direction, ChessBoard board) : 
+        public PawnStartMoveRule(IMoveHistory history, Point movement, Point direction, ChessBoard board) : 
             base(direction, board)
         {
             this.history = history;
+            this.movement = movement;
         }
 
         public override Move CreateMove(Point startPosition, Point endPosition)
@@ -27,6 +29,9 @@ namespace ChessGame.MoveRules
 
         protected override bool IsMoveValid(Point startPosition, Point endPosition)
         {
+            if (new Point(startPosition.X + movement.X, startPosition.Y + movement.Y) != endPosition)
+                return false;
+
             if (history.WasMoved(board[startPosition.X, startPosition.Y]))
                 return false;
 
