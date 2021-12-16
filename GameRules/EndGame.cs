235cbfe373710d09a43d6509a977848c83503aab
ftTnemurtsni <ftTnemurtsni@gmail.com>
2021-12-17 02:36:50 +0300

    feat(GameRules): add basic chess rules

---
+++ GameRules/EndGame.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.GameRules
+{
+    class EndGame
+    {
+        protected ChessBoard board;
+        protected PieceAttacked whiteKingAttacked;
+        protected PieceAttacked blackKingAttacked;
+
+        public EndGame(ChessPiece whiteKing, ChessPiece blackKing, ChessBoard board)
+        {
+            this.board = board;
+            whiteKingAttacked = new PieceAttacked(whiteKing, board);
+            blackKingAttacked = new PieceAttacked(blackKing, board);
+        }
+
+        public bool Turn(Side side)
+        {
+            return SideHasMoves(side);
+        }
+
+        private bool SideHasMoves(Side side)
+        {
+            var kingAttacked = side == Side.White ? whiteKingAttacked : blackKingAttacked;
+            for (int i = 0; i < board.Size; i++)
+            {
+                for(int j = 0; j < board.Size; j++)
+                {
+                    if (board[i, j] != null && board[i, j].Side == side)
+                    {
+                        foreach(var moveRule in board[i, j].Rules)
+                        {
+                            foreach(var move in moveRule.AvailableMoves(new System.Drawing.Point(i, j)))
+                            {
+                                move.Execute();
+                                if (!kingAttacked.IsApplied)
+                                {
+                                    move.Undo();
+                                    return true;
+                                }
+                                move.Undo();
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ GameRules/PieceAttacked.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.GameRules
+{
+    class PieceAttacked
+    {
+        protected ChessPiece piece;
+        protected ChessBoard board;
+
+        public PieceAttacked(ChessPiece piece, ChessBoard board)
+        {
+            this.piece = piece;
+            this.board = board;
+        }
+
+
+        public bool IsApplied
+        {
+            get
+            {
+                Side attackSide = piece.Side == Side.White ? Side.Black : Side.White;
+                System.Drawing.Point? piecePosition = board.FindPiece(piece);
+                if (piecePosition == null)
+                    throw new Exception("Piece for attack cheking not found!");
+                return board.IsUnderAttack((System.Drawing.Point)piecePosition, attackSide);
+            }
+        }
+    }
+}
