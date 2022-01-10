cdbb302ffe7fe0590f78757fcd805d5ffa1e8f79
ftTnemurtsni <ftTnemurtsni@gmail.com>
2022-01-10 23:05:35 +0300

    feat(GameRules): add fifty moves rule

---
+++ GameRules/FiftyMoves.cs
@@ -0,0 +1,29 @@
+﻿using ChessGame.DefaultPieces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.GameRules
+{
+    class FiftyMoves
+    {
+        int movesCount = 0;
+
+        public int MovesCount => movesCount;
+
+        public FiftyMoves(int movesCount = 0)
+        {
+            this.movesCount = movesCount;
+        }
+
+        public void RegisterMove(Move mv)
+        {
+            if (mv.Attacked || mv.Piece is Pawn)
+                movesCount = 0;
+            else
+                movesCount++;
+        }
+    }
+}
