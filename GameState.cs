54e452c143c8daebd446f670aa9c845f6cbc6414
ftTnemurtsni <ftTnemurtsni@gmail.com>
2022-01-11 14:36:09 +0300

    feat(GameState): add GameState for save/reset base game state

---
+++ GameState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame
+{
+    class GameState
+    {
+        public Side Turn { get; private set; }
+
+        public int FiftyMovesCount { get; private set; }
+
+        Action<Side, int> apply;
+
+        public GameState(Side turn, int fiftyMovesCount, Action<Side, int> apply)
+        {
+            Turn = turn;
+            FiftyMovesCount = fiftyMovesCount;
+        }
+
+        public void Apply()
+        {
+            apply(Turn, FiftyMovesCount);
+        }
+    }
+}
